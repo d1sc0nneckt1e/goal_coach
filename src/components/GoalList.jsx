6e144cf493cf8c211d276483839ad25175fbc261
d1sc0nneckt1e <a.c.rezai@gmail.com>
2018-02-22 21:30:31 +0100

    removing a goal implemented

--- src/components/GoalList.jsx
+++ src/components/GoalList.jsx
@@ -5,21 +5,23 @@ import { setGoals } from '../actions';
 import GoalItem from './GoalItem';
 
 class GoalList extends Component{
-  
+
   componentDidMount(){
     goalRef.on('value', snap => {
       let goals = [];
       snap.forEach(goal => {
         const { email, title } = goal.val();
-        goals.push({email, title});
+        const serverKey = goal.key;
+        goals.push({email, title, serverKey});
+        console.log('goal', goal, 'serverKey', serverKey);
       })
-      console.log('goals', goals);
+      // console.log('goals', goals);
       this.props.setGoals(goals);
     })
   }
 
   render(){
-    console.log('this.props.goals', this.props.goals);
+    // console.log('this.props.goals', this.props.goals);
     return(
       <div>
         {
